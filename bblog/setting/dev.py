47c7887fdce08c74e2ded3d98f7bb848e215254b
xiangyufan <xiangyufan@juwang.cn>
2018-03-02 11:41:48 +0800

    基本配置

--- bblog/setting/dev.py
+++ bblog/setting/dev.py
@@ -1,1 +1,15 @@
 from .default import *
+
+
+# 数据库
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'bblog',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'root',
+        'PASSWORD': 'Haishen',
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '3306',                      # Set to empty string for default.
+    }
+}
