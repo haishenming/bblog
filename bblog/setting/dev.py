4625f532db5cd3f46768dcab364715904a49906d
xiangyufan <xiangyufan@juwang.cn>
2018-03-05 11:07:54 +0800

    基本框架

--- bblog/setting/dev.py
+++ bblog/setting/dev.py
@@ -4,12 +4,11 @@ from .default import *
 # 数据库
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'bblog',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bblog',
         'USER': 'root',
         'PASSWORD': 'Haishen',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '3306',                      # Set to empty string for default.
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
--- bblog/urls.py
+++ bblog/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('auth.urls')),              # 认证
+    path('article/', include('article.urls')),        # 文章
+    path('comment/', include('comment.urls')),        # 评论点赞
+    path('/', include('display.urls')),               # 页面显示
+
 ]
