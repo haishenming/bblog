b3c2b665a4c3d301b90ee8d9b0ce3daa7171567f
xiangyufan <xiangyufan@juwang.cn>
2018-03-09 15:17:06 +0800

    home

--- app_article/models.py
+++ app_article/models.py
@@ -24,6 +24,7 @@ class Article(models.Model):
 class Tag(models.Model):
     """ 标签 """
     id = models.IntegerField(primary_key=True)
+    word = models.CharField(max_length=64, null=False, default="")
     user = models.ForeignKey(verbose_name="作者", to='app_users.User',
                                on_delete=models.CASCADE)
     created_time = models.DateTimeField(verbose_name="创建时间", null=False,
@@ -34,6 +35,8 @@ class Category(models.Model):
     id = models.IntegerField(primary_key=True)
     user = models.ForeignKey(verbose_name="作者", to='app_users.User',
                              on_delete=models.CASCADE)
+    word = models.CharField(max_length=64, null=False, default="")
+
     created_time = models.DateTimeField(verbose_name="创建时间", null=False,
                                         auto_now=True)
 
