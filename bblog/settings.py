282ec4f320422bae9198a8ddbbd0f515d0e77fbc
xiangyufan <xiangyufan@juwang.cn>
2018-03-12 11:23:46 +0800

    youbug

--- bblog/settings.py
+++ bblog/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.'
+    # 'django.contrib.',
     'app_users',
     'app_comment',
     'app_article'
